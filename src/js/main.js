a4a91874655d24f39ea83caedaeefd6e38ef6fed
Hakan Bilgin <hbi@longscript.com>
2020-02-04 16:59:31 +0100

    incorrect timeDivision

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 
-import { MidiFile } from "./midi"
+import "./midi-parser.js"
 
-
+const MidiParser = self.MidiParser;
 const NOTES = "c c# d d# e f f# g g# a a# b".split(" ");
 
 
@@ -12,7 +12,9 @@ const synth = {
 		this.progress = window.find(".progress .bar");
 		this.playTime = window.find(".progress .play-time");
 		this.songLength = window.find(".progress .song-length");
+		this.keyboard = window.find(".keyboard");
 
+		this.dispatch({type: "get-temp-file"});
 		//window.midi.init();
 	},
 	async dispatch(event) {
@@ -24,53 +26,98 @@ const synth = {
 			// system events
 			case "open.file":
 				file = await event.open();
-				midi = MidiFile(file.text);
-				console.log(midi);
-				
-				//window.midi.play(file.text);
 
+				self.dispatch({type: "parse-file", file});
+				break;
+			// custom events
+			case "get-temp-file":
+				let request = await defiant.shell(`fs -ur "~/midi/abba.mid"`);
+				file = request.result;
+
+				self.dispatch({type: "parse-file", file});
+				break;
+			case "play-song":
+				if (window.music.playing) {
+					window.music.pause();
+
+					self.progress.css({
+						animationPlayState: "paused",
+					});
+					self.score.css({
+						animationPlayState: "paused",
+					});
+				} else {
+					window.music.play();
+
+					setTimeout(() => {
+						self.progress.css({
+							transform: "translateX(0%)",
+							transitionDuration: (self.songLength) +"ms",
+						});
+						self.score.css({
+							transform: "translateY(0)",
+							transitionDuration: (self.songLength) +"ms",
+						});
+					}, 1);
+				}
+
+				break;
+			case "parse-file":
+				file = event.file;
+
+				midi = MidiParser.parse(file.buffer);
+				console.log(midi);
+return
 				// set window title
 				window.title = `Synth - ${file.name}`;
 
 				// reset score
-				let score = [],
+				let timeline = [],
+					score = [],
 					songLength = 0;
 
-				midi.tracks.map((track, t) => {
+				midi.track.map((track, t) => {
 					let time = 0,
 						record = {};
 
-					track.map(item => {
+					track.event.map(item => {
 						let octave,
-							note,
-							bottom,
-							height;
-
+							event,
+							noteNumber;
+						
 						time += item.deltaTime;
 
-						switch (item.subtype) {
-							case "noteOn":
-								octave = parseInt(item.noteNumber / 12, 10) - 1;
-								note = NOTES[item.noteNumber % 12];
-								bottom = parseInt(time / 10, 10);
-								record[item.noteNumber] = `<b note="${note}" octave="${octave}" track="${t+1}" style="bottom: ${bottom}px; height: endTime;"></b>`;
-								break;
-							case "noteOff":
-								note = record[item.noteNumber];
-								bottom = +note.match(/bottom: (\d{1,})px;/i)[1];
-								height = parseInt(time / 10, 10) - bottom;
-								score.push(note.replace(/endTime/, height +"px"));
-								// delete note from record
-								delete record[item.noteNumber];
-								break;
-							case "setTempo":
-							case "setVolume":
-							case "programChange":
-							case "chordOn":
-							case "chordOff":
-							case "stopAllNotes":
-							case "connect":
-								//console.log(item);
+						switch (item.type) {
+							case 9:
+								noteNumber = item.data[0];
+								if (item.data[1] === 0 && record[noteNumber]) {
+									// note event end
+									event = record[noteNumber];
+									height = parseInt(time / 10, 10) - event.bottom;
+
+									// save html to score array
+									score.push(`<b note="${event.note}" octave="${event.octave}" track="${t+1}" style="bottom: ${event.bottom}px; height: ${height}px;"></b>`);
+									timeline.push({
+										time: event.time,
+										track: event.track,
+										delta: event.delta,
+										duration: (item.deltaTime * 4) - event.delta,
+										el: self.keyboard.find(`[octave="${event.octave}"][note="${event.note}"]`),
+									});
+
+									// delete note event from record
+									delete record[noteNumber];
+								} else {
+									// save note event start
+									record[noteNumber] = {
+										time,
+										track: t+1,
+										delta: item.deltaTime * 4,
+										octave: parseInt(noteNumber / 12, 10) - 1,
+										note: NOTES[noteNumber % 12],
+										bottom: parseInt(time / 10, 10),
+									};
+								}
 								break;
 						}
 					});
@@ -78,28 +125,52 @@ const synth = {
 					if (time > songLength) songLength = time;
 				});
 				
+				timeline = timeline.sort((a, b) => a.time - b.time);
+				// console.log(timeline[0].time, timeline[0].track)
+				// console.log(timeline[1].time, timeline[1].track)
+				// console.log(timeline[2].time, timeline[2].track)
+				// console.log(timeline[3].time, timeline[3].track)
+
+				self.timeline = timeline;
 				self.songLength = songLength; // in milliseconds
 
 				self.score
 					.css({
 						height: (songLength / 10) +"px",
-						transform: `translateY(${-(songLength / 10)}px)`,
+						transform: `translateY(${-(songLength / 10) + 375}px)`,
 					})
 					.html(score.join(""));
-				break;
-			// custom events
-			case "play-song":
+
+				// play music
+				window.music.play(file.buffer);
+
+				setTimeout(() => self.playback(), timeline[0].delta + 700);
+
 				setTimeout(() => {
 					self.progress.css({
 						transform: "translateX(0%)",
 						transitionDuration: (self.songLength) +"ms",
 					});
 					self.score.css({
-						transform: "translateY(0)",
+						transform: "translateY(443px)",
 						transitionDuration: (self.songLength) +"ms",
 					});
-				}, 1);
-				break;
+				}, 0);
+				break;	
+		}
+	},
+	playback() {
+		let event = this.timeline.shift(),
+			className = "active track-"+ event.track,
+			duration = Math.max(event.duration, 80);
+		
+		event.el
+			.prop({ style: `--duration: ${duration}ms;` })
+			//.prop({ style: `--duration: 100ms` })
+			.cssSequence(className, "transitionend", el => el.removeClass(className).prop({ style: "" }));
+
+		if (this.timeline.length) {
+			setTimeout(() => this.playback(), this.timeline[0].delta);
 		}
 	}
 };
