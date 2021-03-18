4ca98a91e65b8e1133e6c464bf177e5e412df2e4
Hakan Bilgin <hbi99@hotmail.com>
2021-03-18 12:16:24 +0100

    keyboard

--- src/js/modules/keyboard.js
+++ src/js/modules/keyboard.js
@@ -1,4 +1,37 @@
 
+const Matrix = [
+	// white keys
+	{ note: "c",  clip: [ 0,   0, 48,  280, 0,   0, 26, 151 ] },
+	{ note: "c#", clip: [ 288, 0, 32,  169, 17,  0, 17, 92  ] }, // black key
+	{ note: "d",  clip: [ 48,  0, 48,  280, 26,  0, 26, 151 ] },
+	{ note: "d#", clip: [ 288, 0, 32,  169, 43,  0, 17, 92  ] }, // black key
+	{ note: "d",  clip: [ 96,  0, 48,  280, 52,  0, 26, 151 ] },
+	{ note: "f",  clip: [ 0,   0, 48,  280, 78,  0, 26, 151 ] },
+	{ note: "f#", clip: [ 288, 0, 32,  169, 95,  0, 17, 92  ] }, // black key
+	{ note: "g",  clip: [ 48,  0, 48,  280, 104, 0, 26, 151 ] },
+	{ note: "g#", clip: [ 288, 0, 32,  169, 121, 0, 17, 92  ] }, // black key
+	{ note: "a",  clip: [ 48,  0, 48,  280, 130, 0, 26, 151 ] },
+	{ note: "a#", clip: [ 288, 0, 32,  169, 147, 0, 17, 92  ] }, // black key
+	{ note: "b",  clip: [ 96,  0, 48,  280, 156, 0, 26, 151 ] },
+];
+
+
+class Key {
+	constructor(octave, note, clip) {
+		// sX, sY, sW, sH, dX, dY, dW, dH
+		this.octave = octave;
+		this.note = note;
+		this.clip = [...clip];
+		// offset key per octave
+		this.clip[4] += octave * 182;
+	}
+
+	serialize() {
+		return this.clip;
+	}
+};
+
+
 const Keyboard = {
 	init() {
 		this.cvs = window.find(".keyboard canvas");
@@ -14,35 +47,26 @@ const Keyboard = {
 		this.sprite.onload = () => this.render();
 		this.sprite.src = "~/img/piano-keys.png";
 
-		// key position matrix
-		this.matrix = [
-			// white keys
-			[ 0,  0, 48, 280, 0,   0, 26, 151 ],
-			[ 48, 0, 48, 280, 26,  0, 26, 151 ],
-			[ 96, 0, 48, 280, 52,  0, 26, 151 ],
-			[ 0,  0, 48, 280, 78,  0, 26, 151 ],
-			[ 48, 0, 48, 280, 104, 0, 26, 151 ],
-			[ 48, 0, 48, 280, 130, 0, 26, 151 ],
-			[ 96, 0, 48, 280, 156, 0, 26, 151 ],
-			// black keys
-			[ 32, 0, 32, 170, 17,  0, 17, 92  ],
-			[ 32, 0, 32, 170, 43,  0, 17, 92  ],
-			[ 32, 0, 32, 170, 95,  0, 17, 92  ],
-			[ 32, 0, 32, 170, 121, 0, 17, 92  ],
-			[ 32, 0, 32, 170, 147, 0, 17, 92  ],
-		];
+		this.keys = [];
+		[...Array(6)].map((i, octave) => {
+			Matrix.map(item => {
+				this.keys.push(new Key( octave, item.note, item.clip ));
+			});
+		});
 	},
 	render() {
 		let sprite = this.sprite;
 
-		[...Array(6)].map((i, octave) => {
-			console.log(octave);
-		});
-		this.matrix.map(key => {
-			let params = [sprite].concat(key);
+		this.keys.map(key => {
+			let params = [sprite].concat(key.serialize());
 			this.ctx.drawImage(...params);
 		});
 
+		// Matrix.map(key => {
+		// 	let params = [sprite].concat(key);
+		// 	this.ctx.drawImage(...params);
+		// });
+		
 		/*
 		// white keys
 		this.ctx.drawImage(sprite, 0,  0, 48, 280, 0,  0, 26, 151);
