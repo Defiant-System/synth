430ffc22a2354e2bf62012cc348340a863e6121d
Hakan Bilgin <hbi99@hotmail.com>
2021-03-18 12:46:26 +0100

    score guidelines

--- src/js/modules/score.js
+++ src/js/modules/score.js
@@ -3,10 +3,29 @@ const Score = {
 	init() {
 		this.cvs = window.find(".score canvas");
 		this.ctx = this.cvs[0].getContext("2d");
-		// set canvas dimensions
-		this.cvs.prop({
+		this.dim = {
 			width: this.cvs.prop("offsetWidth"),
 			height: this.cvs.prop("offsetHeight")
+		};
+		// set canvas dimensions
+		this.cvs.prop(this.dim);
+
+		this.guideLines = [];
+		[...Array(6)].map((i, octave) => {
+			this.guideLines.push({ x: -.5 + (octave * 182),  color: "#404040" });
+			this.guideLines.push({ x: 77.5 + (octave * 182), color: "#383838" });
+		});
+
+		this.render();
+	},
+	render() {
+		this.guideLines.map(line => {
+			this.ctx.strokeStyle = line.color;
+
+			this.ctx.beginPath();
+			this.ctx.moveTo(line.x, 0);
+			this.ctx.lineTo(line.x, this.dim.height);
+			this.ctx.stroke();
 		});
 	}
 };
