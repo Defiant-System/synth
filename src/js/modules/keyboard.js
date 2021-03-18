6829f5643893827c142c192561a32d9636d11249
Hakan Bilgin <hbi99@hotmail.com>
2021-03-18 12:31:09 +0100

    better key matrix

--- src/js/modules/keyboard.js
+++ src/js/modules/keyboard.js
@@ -1,18 +1,18 @@
 
 const Matrix = [
 	// white keys
-	{ note: "c",  clip: [ 0,   0, 48,  280, 0,   0, 26, 151 ] },
-	{ note: "c#", clip: [ 288, 0, 32,  169, 17,  0, 17, 92  ] }, // black key
-	{ note: "d",  clip: [ 48,  0, 48,  280, 26,  0, 26, 151 ] },
-	{ note: "d#", clip: [ 288, 0, 32,  169, 43,  0, 17, 92  ] }, // black key
-	{ note: "d",  clip: [ 96,  0, 48,  280, 52,  0, 26, 151 ] },
-	{ note: "f",  clip: [ 0,   0, 48,  280, 78,  0, 26, 151 ] },
-	{ note: "f#", clip: [ 288, 0, 32,  169, 95,  0, 17, 92  ] }, // black key
-	{ note: "g",  clip: [ 48,  0, 48,  280, 104, 0, 26, 151 ] },
-	{ note: "g#", clip: [ 288, 0, 32,  169, 121, 0, 17, 92  ] }, // black key
-	{ note: "a",  clip: [ 48,  0, 48,  280, 130, 0, 26, 151 ] },
-	{ note: "a#", clip: [ 288, 0, 32,  169, 147, 0, 17, 92  ] }, // black key
-	{ note: "b",  clip: [ 96,  0, 48,  280, 156, 0, 26, 151 ] },
+	{ note: "c",  clip: [ 0,   0, 48, 280, 0,   0, 26, 151 ] },
+	{ note: "c#", clip: [ 144, 0, 32, 169, 17,  0, 17, 92  ] }, // black key
+	{ note: "d",  clip: [ 48,  0, 48, 280, 26,  0, 26, 151 ] },
+	{ note: "d#", clip: [ 144, 0, 32, 169, 43,  0, 17, 92  ] }, // black key
+	{ note: "d",  clip: [ 96,  0, 48, 280, 52,  0, 26, 151 ] },
+	{ note: "f",  clip: [ 0,   0, 48, 280, 78,  0, 26, 151 ] },
+	{ note: "f#", clip: [ 144, 0, 32, 169, 95,  0, 17, 92  ] }, // black key
+	{ note: "g",  clip: [ 48,  0, 48, 280, 104, 0, 26, 151 ] },
+	{ note: "g#", clip: [ 144, 0, 32, 169, 121, 0, 17, 92  ] }, // black key
+	{ note: "a",  clip: [ 48,  0, 48, 280, 130, 0, 26, 151 ] },
+	{ note: "a#", clip: [ 144, 0, 32, 169, 147, 0, 17, 92  ] }, // black key
+	{ note: "b",  clip: [ 96,  0, 48, 280, 156, 0, 26, 151 ] },
 ];
 
 
@@ -26,8 +26,17 @@ class Key {
 		this.clip[4] += octave * 182;
 	}
 
+	press(state) {
+		this.state = state;
+	}
+
 	serialize() {
-		return this.clip;
+		let clip = [...this.clip];
+
+		// move clip area
+		if (this.state === "down") clip[0] += 176;
+		
+		return clip;
 	}
 };
 
@@ -39,7 +48,7 @@ const Keyboard = {
 		// set canvas dimensions
 		this.cvs.prop({
 			width: this.cvs.prop("offsetWidth"),
-			height: this.cvs.prop("offsetHeight")
+ 			height: this.cvs.prop("offsetHeight")
 		});
 
 		// image sprite
@@ -53,6 +62,8 @@ const Keyboard = {
 				this.keys.push(new Key( octave, item.note, item.clip ));
 			});
 		});
+
+		// this.keys[16].press("down");
 	},
 	render() {
 		let sprite = this.sprite;
@@ -61,30 +72,5 @@ const Keyboard = {
 			let params = [sprite].concat(key.serialize());
 			this.ctx.drawImage(...params);
 		});
-
-		// Matrix.map(key => {
-		// 	let params = [sprite].concat(key);
-		// 	this.ctx.drawImage(...params);
-		// });
-		
-		/*
-		// white keys
-		this.ctx.drawImage(sprite, 0,  0, 48, 280, 0,  0, 26, 151);
-		this.ctx.drawImage(sprite, 48, 0, 48, 280, 26, 0, 26, 151);
-		this.ctx.drawImage(sprite, 96, 0, 48, 280, 52, 0, 26, 151);
-
-		this.ctx.drawImage(sprite, 0,  0, 48, 280, 78,  0, 26, 151);
-		this.ctx.drawImage(sprite, 48, 0, 48, 280, 104, 0, 26, 151);
-		this.ctx.drawImage(sprite, 48, 0, 48, 280, 130, 0, 26, 151);
-		this.ctx.drawImage(sprite, 96, 0, 48, 280, 156, 0, 26, 151);
-
-		// black keys
-		this.ctx.drawImage(sprite, 32, 0, 32, 170, 17, 0, 17, 92);
-		this.ctx.drawImage(sprite, 32, 0, 32, 170, 43, 0, 17, 92);
-
-		this.ctx.drawImage(sprite, 32, 0, 32, 170, 95,  0, 17, 92);
-		this.ctx.drawImage(sprite, 32, 0, 32, 170, 121, 0, 17, 92);
-		this.ctx.drawImage(sprite, 32, 0, 32, 170, 147, 0, 17, 92);
-		*/
 	}
 };
